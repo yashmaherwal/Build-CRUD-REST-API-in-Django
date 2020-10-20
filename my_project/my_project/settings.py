716d806e1f989ab293f7f22dcb1fbfb098267e55
tarabacha <56684364+tarabacha@users.noreply.github.com>
2020-10-21 00:08:36 +0530

    Update settings.py
    Static DIR added

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+# TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+# STATIC_DIR  = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
