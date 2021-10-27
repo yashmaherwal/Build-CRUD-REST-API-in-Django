e0537d7080e12d2bc64740afcd196e25cae6bbf0
starbacha <73193663+starbacha@users.noreply.github.com>
2021-10-27 19:44:58 +0530

    Update settings.py
    Update to include dotenv and hide important info

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+from dotenv impot load_dotenv
+load_dotenv()
 # Build paths inside the project exactly like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
