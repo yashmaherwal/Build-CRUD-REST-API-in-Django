40329c8cdef5ac2fbd438949f7f7d88c5d9d1998
yashmaherwal <72273482+yashmaherwal@users.noreply.github.com>
2022-10-19 13:02:54 +0530

    Update wsgi.py

--- my_project/my_project/wsgi.py
+++ my_project/my_project/wsgi.py
@@ -2,7 +2,7 @@
 WSGI config for my_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
-got it?
+Information guide-
 For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
