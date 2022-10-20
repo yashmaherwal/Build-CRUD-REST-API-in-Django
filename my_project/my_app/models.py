719f2baba10b02ae4223ec26324b75770ac6412a
Vishu Tyagi <39157981+strider187@users.noreply.github.com>
2022-10-20 15:01:25 +0530

    Update models.py
    add the str configuration of the table

--- my_project/my_app/models.py
+++ my_project/my_app/models.py
@@ -7,3 +7,6 @@ from django.db import models
 class Employee(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=30)
+    
+    def __str__():
+        return "employee_table"
